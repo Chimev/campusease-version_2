26f0a63b32478771b3b0a23ded99f3e9710d72bb
Chimev <chimevictor.CV@gmail.com>
2024-08-26 13:16:30 +0100

    try agai8n removed export from handler function in routes .ts in app auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "./auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST};
 
 
