18e961c5b87d8d61e441bab356d3937df9e3bb7f
Chimev <chimevictor.CV@gmail.com>
2024-08-26 11:13:50 +0100

    added auth..s to the auth process

---
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,44 @@
+import { NextAuthOptions } from "next-auth";// ts
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+import User from '@/utilis/models/User';
+import  {connectToDB}  from '@/utilis/connectToDB';
+import bcrypt from 'bcryptjs';
+
+export const authOptions: NextAuthOptions = {
+    // Configure one or more authentication providers
+    providers: [
+      CredentialsProvider({
+        id: "credentials",
+        name: "Credentials",
+        credentials: {
+          email: {label: "Email", type: "email"},
+          password: {label: "Password", type: "password"},
+        },
+        async authorize(credentials: any) {
+          await connectToDB();
+          try {
+            const user = await User.findOne({email: credentials.email});
+            if(user) {
+              const isPasswordCorrect = await bcrypt.compare(
+                credentials.password,
+                user.password
+              )
+              if(isPasswordCorrect) {
+                return user;
+              }
+            }
+            return null;
+          } catch (error: any) {
+            throw new Error(error);
+          }
+        }
+      }),
+      GithubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+      }),
+      // ...add more providers here
+    ],
+  }
+
