bb0b8e29b5a3f9593cd20badfb5d0ac0498cbf62
Chimev <chimevictor.CV@gmail.com>
2024-11-23 20:52:39 +0100

    must be sign in beforew any operation

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,9 +1,17 @@
 import ListPage from '@/components/listPage/listPage'
 import { FavouriteListProvider } from '@/lib/Context/FavoriteContext'
 import { SchoolContextProvider } from '@/lib/Context/SchholContext'
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession();
+
+  if(!session) {
+    redirect('/sign-in');
+  }
+
   return (
     <SchoolContextProvider>
       <FavouriteListProvider>
