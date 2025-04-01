e61a39acf3ae96b318c0b1f40e8bb4459febb1a2
Chimev <chimevictor.CV@gmail.com>
2025-04-01 14:11:53 +0100

    total users admin

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { MdChurch } from "react-icons/md";
 
 const Page = async () => {
   const users = await getUsers()
+  console.log(users.totalUsers)
   const listings = await getListings()
   const schools = await getSchools()
  
@@ -14,7 +15,7 @@ const Page = async () => {
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-3  gap-6'>
-      <MetricCard metricTitle ={'Total Users'} icon={<MdGroups />} number={users.length}  />
+      <MetricCard metricTitle ={'Total Users'} icon={<MdGroups />} number={users.totalUsers}  />
       <MetricCard metricTitle ={'Listings'} icon={<MdGroups />} number={listings.length}  />
       <MetricCard metricTitle ={'Schools'} icon={<MdGroups />} number={schools.length}  />
     </div>
