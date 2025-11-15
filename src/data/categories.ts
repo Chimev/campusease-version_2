eeb8eb5859dca8fe6b03e62f4643d4131f84f1a0
Chimev <chimevictor.CV@gmail.com>
2025-11-15 13:07:47 +0100

    corrected category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,6 +1,6 @@
 export const categories = [
     {link:'/category/accommodation', category:'Accommodation', description: "Find Accomodation within your school", img:"/Rent.jpg", tagline: 'Find your perfect space' },
-    {link:'/category/roommate', category:'Roommate', description: "Find roommates within your school", img:"/Roommate.jpg", tagline: 'Meet your ideal match' },
-    {link:'/category/service', category:'Service', description:"Find Service provider around your school", img:"/Service.png", tagline: 'Buy, sell, save' },
+    {link:'/category/roommates', category:'Roommates', description: "Find roommates within your school", img:"/Roommate.jpg", tagline: 'Meet your ideal match' },
+    {link:'/category/services', category:'Services', description:"Find Service provider around your school", img:"/Service.png", tagline: 'Buy, sell, save' },
     {link:'/category/marketplace', category:'MarketPlace', description: "Buy/Sell/Swap within your school", img:"/Trade.jpg", tagline: 'Stay connected' },
   ]
